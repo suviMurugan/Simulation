8e068571946d7004e7052560dc31f81c546b9f07
Suvi Murugan <suvim12199@gmail.com>
2013-07-23 18:03:15 +0900

    Color of Graph update
    updated with ability to update with up to 7 colors

--- SimuEpiPhase.m
+++ SimuEpiPhase.m
@@ -1,4 +1,3 @@
-
 function population=SimuEpiPhase(population,rateVector,iteration)
 
 %The program is done to simulate the epidemics with phases of infection
@@ -13,17 +12,16 @@ function population=SimuEpiPhase(population,rateVector,iteration)
  end
  infectRate=rateVector(1);
  
+colorcode=['y','m','c','r','g','b','k'];
+
 for i=1:iteration
     rateVector(1)=infectRate*population(1)*(1-population(1))
     rate = rateMatrixGen(rateVector);
     temp=population;
     population = population * rate;
     hold on;
-    %for j = 1:length(population) 
-    plot([i-1 i],[temp(1) population(1)], 'g');
-    plot([i-1 i],[temp(2) population(2)], 'r');
-    plot([i-1 i],[temp(3) population(3)], 'm');
-    plot([i-1 i],[temp(4) population(4)], 'b');
-    plot([i-1 i],[temp(5) population(5)], 'y');
+    for j = 1:length(population) 
+        plot([i-1 i],[temp(j) population(j)],colorcode(j));
+    end
 end
 hold off
