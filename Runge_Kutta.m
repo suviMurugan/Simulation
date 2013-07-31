3c203a029f067c10f3cc77a1cfee2a74cd934a7a
Suvi Murugan <suvim12199@gmail.com>
2013-07-31 22:21:16 +0900

    Update Runge_Kutta.m
    plots curve than points.

--- Runge_Kutta.m
+++ Runge_Kutta.m
@@ -5,8 +5,6 @@ t(i) = 0;                                                           %Initial tim
 w(i) = 0.01;                                                        %Initial value of the function to be plotted.
 fprintf('Step 0: t = %12.8f, w = %12.8f\n', t(i), w(i));
 for i=1:100                                                         %Iteration for 100 times
-    plot(t(i),w(i));                                                %Plot the point in every instance.
-    hold on;
     k1 = h*Infect(t(i),w(i));
     k2 = h*Infect(t(i)+h/2, w(i)+k1/2);
     k3 = h*Infect(t(i)+h/2, w(i)+k2/2);
@@ -15,6 +13,7 @@ for i=1:100                                                         %Iteration f
     t(i+1) = t(i) + h;
     fprintf('Step %d: t = %6.4f, w = %18.15f\n', i, t(i+1), w(i+1));
 end
+plot(t,w);                                                          %plot the approximated curve
 %%%%%%%%%%%%%%%%%%
 function v = Infect(t,I)
     v = 2*I + t;                                                    %can add any function to be plotted.
