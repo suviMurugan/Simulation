6f8698728c36da302aa9a38c68efaf3e68713eec
aliasneo1 <sriram.india1@gmail.com>
2013-08-16 16:54:05 +0530

    Update GenerateBA.m
    slow for n>500...

--- GenerateBA.m
+++ GenerateBA.m
@@ -2,34 +2,39 @@ function graph = GenerateBA(NumNode,MinDeg)
 
 %The code is done to generate BA model of graph, preferential attachment
 %till now Mindeg not included
-%code is buggy but produces result on prob bases. if gets stuck in some
-%error of index out of bound run it again, it may run in the next run.
-%Probabilistic error still to be debugged.
+%probability bug removed but its output is slow for large n !
+
 
 %This is for the undirected one.
 
 graph=zeros(2,2);                                                          % creates the initial 2 node graph
 graph(1,2)=1;
 graph(2,1)=1;
-deg = 0;                                                                   %Initialize degree to 0 but not needed now
-
-    for i=1:NumNode                                                        %loop to add NumNode number of Nodes
+deg = 0;  %Initialize degree to 0 but not needed now
+n=3;
+    for i=4:NumNode+1               %loop to add NumNode number of Nodes
         cumulative = 0;
-            for k=1:unique(size(graph))                                    %calculates the prob for each node and keeps it's cumulative value in val
+            for k=1:unique(size(graph)) %calculates the prob for each node and keeps it's cumulative value in val
                 prob(k)=nnz(graph(k,:))/nnz(graph);
                 val(k)=cumulative + prob(k);
                 cumulative = cumulative + prob(k);
-            end 
-            for j=i-1:-1:1                                                 % the allocation of edge based on the prob value.
+            end
+         
+              for in=1:inf        
+                if n<i             % loops untill an node is added
                 edgeProb=rand();
                 for k=1:unique(size(graph))
-                     if(val(k) > edgeProb && j~=k)                         % to pervent self loop and check with prob value.
-                         graph(j,k)=1;
-                        graph(k,j)=1;
+                     if(val(k) > edgeProb && j~=k) % to pervent self loop and check with prob value.
+                         graph(n,k)=1;
+                        graph(k,n)=1;
+                        n=n+1;     
                       break;
                      end
                 end
-            end
+                else
+                    break;
+                end
+              end
         graph                                                              %prints the adjacency matrix.
     end
     for i = 1 : unique(size(graph))                                        %find degree distribution
