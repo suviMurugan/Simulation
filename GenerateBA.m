d9ecf5a14df614d0df1b39ed8594d3a69d5e60a2
Suvi Murugan <suvim12199@gmail.com>
2013-08-19 21:01:21 +0900

    update code on 201308199
    The probabilistic bug is debugged.

--- GenerateBA.m
+++ GenerateBA.m
@@ -1,13 +1,9 @@
-function graph = GenerateBA(NumNode,MinDeg)
-
-%The code is done to generate BA model of graph, preferential attachment
+function graph=GenerateBA(NumNode,MinDeg)
+%the code is done to generate BA model of graph, preferential attachment
 %till now Mindeg not included
 %code is buggy but produces result on prob bases. if gets stuck in some
 %error of index out of bound run it again, it may run in the next run.
 %Probabilistic error still to be debugged.
-
-%This is for the undirected one.
-
 graph=zeros(2,2);                                                          % creates the initial 2 node graph
 graph(1,2)=1;
 graph(2,1)=1;
@@ -15,26 +11,26 @@ deg = 0;                                                                   %Init
 
     for i=1:NumNode                                                        %loop to add NumNode number of Nodes
         cumulative = 0;
-            for k=1:unique(size(graph))                                    %calculates the prob for each node and keeps it's cumulative value in val
-                prob(k)=nnz(graph(k,:))/nnz(graph);
-                val(k)=cumulative + prob(k);
-                cumulative = cumulative + prob(k);
-            end 
-            for j=i-1:-1:1                                                 % the allocation of edge based on the prob value.
-                edgeProb=rand();
-                for k=1:unique(size(graph))
-                     if(val(k) > edgeProb && j~=k)                         % to pervent self loop and check with prob value.
-                         graph(j,k)=1;
-                        graph(k,j)=1;
-                      break;
-                     end
-                end
+        for k=1:unique(size(graph))                                        %calculates the prob for each node and keeps it's cumulative value in val
+            prob(k)=nnz(graph(k,:))/nnz(graph);
+            val(k)=cumulative + prob(k);
+            cumulative = cumulative + prob(k);
+        end 
+          value_length=unique(size(graph))
+        for j=i-1:-1:1                                                     % the allocation of edge based on the prob value.
+            edgeProb=rand();
+            for k=1:value_length;
+                 if(val(k) >= edgeProb && j~=k)                             % to pervent self loop and check with prob value.
+                     graph(j,k)=1;
+                     graph(k,j)=1;
+                   break;
+                 end
             end
-        graph                                                              %prints the adjacency matrix.
+        end
+        graph
     end
-    for i = 1 : unique(size(graph))                                        %find degree distribution
+    for i = 1 : unique(size(graph)) %find degree distribution
         x(i) = nnz (graph(i,:));
-    end 
+    end
     plot(1:unique(size(graph)),x)                                          % plot the degree distribution
 end
-
