c9380a8d6ee661b18d726ad81486af3e6aacd37e
Suvi Murugan <suvim12199@gmail.com>
2013-08-23 21:12:54 +0900

    Update after working scalefree
    This one is updated with the graph as the input.
    Spreading processes works perfect.

--- GraphEpiSim.m
+++ GraphEpiSim.m
@@ -1,23 +1,21 @@
 %To incoporate the network model for epidemic analysis.
-function result=GraphEpiSim(NumNode,InitialInfection,Levels)
-%network as Adjacency Matrix first want to model spread.
-Mat= GenerateGraph();                                          %Generation of Random Network
+function result=GraphEpiSim(Graph,InitialInfection,Levels)
+%If a graph(adjacency matrix) with number of initial infected, 
+%with how many levels the virus must spread. gives you the result 
+%as node and the level they were infected with 0 means not infected.
+NumNode = unique(size(Graph));                                 %Size of network is used
 Suscep=zeros(1,NumNode);                                       %Initaillsing All population as Susceptible
-
                                                                %Initial Infection of nodes
 Initial = round(rand(1,InitialInfection)*NumNode);
 Suscep(Initial) = 1;
-
                                                                %Initialization for infection
 Inf = Initial;
 iteration=1;
 allN=Initial(1);
                                                                %Iteration of spreading in the network.
-
 while(iteration < Levels)
-    
     for i=1:length(Inf)
-        neig = find(Mat(Inf(i),:) ~= 0);
+        neig = find(Graph(Inf(i),:) ~= 0);
         allN = [allN neig];
     end
     uniN = unique(allN);
@@ -25,7 +23,6 @@ while(iteration < Levels)
     Suscep(Inf)=iteration;
     Suscep
     iteration=iteration+1;
-    
 end
 result=nnz(Suscep);
 end
