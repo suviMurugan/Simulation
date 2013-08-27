39b9a65c3c9f126767285f779b73940690e15152
Suvi Murugan <suvim12199@gmail.com>
2013-08-27 15:18:56 +0900

    Code to Generate BA model graph.
    Fully working Barabasi-Albert Graph generator in Matlab.

--- GenerateBA.m
+++ GenerateBA.m
@@ -1,13 +1,23 @@
 function graph=GenerateBA(NumNode,MinDeg)
 %the code is done to generate BA model of graph, preferential attachment
-  graph=[0 1; 1 0];                                                        %creates the initial 2 node graph
+  graph=[0 1; 1 0];                                                        % creates the initial 2 node graph
     for i=1:NumNode                                                        %loop to add NumNode number of Nodes
         val =cumsum( sum(graph,2)/sum(sum(graph)) );                       %finding cumulative probability value   
-           for in=1:MinDeg                                                 %mindeg number of nodes added.
-              edgeProb=rand();                                             %generate random value
+              edgeProb=rand();                                             %for first node seperately generate random value
               pos=find( (val - edgeProb) >= 0 );                           %find the selected nodefrom the cumsum val.
               graph(i+1,pos(1))=1;                                         %create the edges for undirected
               graph(pos(1),i+1)=1;                                         %remove this to make it directed
+                    
+           for in=2:MinDeg                                                 % mindeg number of nodes added.
+              edgeProb=rand();                                             %generate random value
+              pos=find( (val - edgeProb) >= 0 );                           %find the selected nodefrom the cumsum val.
+               for k=1:length(pos)
+                if( graph(i+1,pos(k))~= 1)
+                    graph(i+1,pos(k))=1;                                   %create the edges for undirected
+                    graph(pos(k),i+1)=1;                                   %remove this to make it directed
+                    break
+                end
+               end
            end
     end
   x = sum(graph,2);                                                        %finding the deg value of each node.
